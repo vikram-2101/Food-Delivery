79ad003917c07c08c4b2db96fcece3862d556a81
Vikram Kumar <145278357+vikram-2101@users.noreply.github.com>
2024-08-04 14:41:32 +0530

    Update StoreContext.jsx

--- Frontend/src/context/StoreContext.jsx
+++ Frontend/src/context/StoreContext.jsx
@@ -5,7 +5,7 @@ export const StoreContext = createContext(null);
 const StoreContextProvider = (props) => {
   const [food_list, setFoodList] = useState([]);
   const [cartItems, setCartItems] = useState({});
-  const url = "http://localhost:4000";
+  const url = "https://food-delivery-backend-kwue.onrender.com";
   const [token, setToken] = useState("");
   const addToCart = async (itemId) => {
     if (!cartItems[itemId]) {
